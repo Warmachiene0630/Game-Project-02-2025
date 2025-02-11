51dc17d4c192101044dd9eeb03521ea117d32d53
Warmachiene0630 <lmeza0630@gmail.com>
2025-02-11 03:24:13 -0700

    Late night coding

---
+++ ProjectGame/Project Game/Assets/Scripts Luis/Movable Wall.cs
@@ -0,0 +1,57 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovableWall : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+
+    private float trapTimer;
+    [SerializeField] int trapRate;
+    [SerializeField] int trapDelay;
+    [SerializeField] int trapSpeed;
+
+    private Vector3 playerDistance;
+    private bool trapMoved;
+    [SerializeField] int trapTrigger;
+
+    Vector3 trapStart;
+
+    private bool active;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDistance = transform.position;
+
+        if((playerDistance.y <= trapTrigger || playerDistance.x <= trapTrigger || playerDistance.z <= trapTrigger))
+        {
+            moveTrap();
+        }
+
+    }
+
+    private void moveTrap()
+    {
+        if (trapMoved == false && trapTimer >= trapRate)
+        {
+            Quaternion rot = Quaternion.LookRotation(new Vector3(transform.position.x + 3, 0, 0));
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * trapSpeed);
+            trapMoved = !trapMoved;
+        }
+        else
+        {
+            Quaternion rot = Quaternion.LookRotation(new Vector3(transform.position.x - 3, 0, 0));
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * trapSpeed);
+            if (transform.position == trapStart)
+            {
+                trapMoved = !trapMoved;
+            }
+        }
+    }
+}
--- ProjectGame/Project Game/Assets/Scripts Luis/Traps.cs
+++ ProjectGame/Project Game/Assets/Scripts Luis/Traps.cs
@@ -3,47 +3,37 @@ using UnityEngine.AI;
 
 public class Traps : MonoBehaviour
 {
-    enum trapType {Moving, Stationary, Shooter}
+    enum trapType {Stationary, Shooter}
     [SerializeField] trapType type;
     [SerializeField] Rigidbody rb;
 
     private float trapTimer;
-    [SerializeField] int trapRate;
-    [SerializeField] int trapDelay;
-    [SerializeField] int trapSpeed;
-
+    [SerializeField] float trapRate;
 
     [SerializeField] Transform shootPos;
-    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject cannonBall;
 
     private Vector3 playerDistance;
-    private bool trapMoved;
     [SerializeField] int trapTrigger;
-    [SerializeField] int destroyTimer;
 
     [SerializeField] int damageAmount;
-    [SerializeField] int destroyDelay;
 
     Vector3 trapStart;
 
     private bool active;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Update()
     {
-        
+          trapTimer += Time.deltaTime;
+        if (trapTimer >= trapRate)
+        {
+            shoot();
+        }
     }
+  
 
     // Update is called once per frame
-    void Update()
-    {
-        playerDistance = gamemanager.instance.player.transform.position - transform.position;
-        
-        if (type == trapType.Moving && ((playerDistance.y <= trapTrigger || playerDistance.x <= trapTrigger || playerDistance.z <= trapTrigger) || trapMoved == true))
-        {
-            moveTrap();
-        }
-
-    }
     private void OnTriggerEnter(Collider other)
     {
         IDamage dmg = other.GetComponent<IDamage>();
@@ -59,30 +49,12 @@ public class Traps : MonoBehaviour
         }
     }
 
-    private void moveTrap()
-    {
-        if (trapMoved == false && trapTimer >= trapRate)
-        {
-            Quaternion rot = Quaternion.LookRotation(new Vector3(transform.position.x + 3, 0, 0));
-            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * trapSpeed);
-            trapMoved = !trapMoved;
-        }
-        else
-        {
-            Quaternion rot = Quaternion.LookRotation(new Vector3(transform.position.x - 3, 0, 0));
-            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * trapSpeed);
-            if (transform.position == trapStart)
-            {
-                trapMoved = !trapMoved;
-            }
-        }
-    }
 
     void shoot()
     {
         trapTimer = 0;
 
-        Instantiate(bullet, shootPos.position, transform.rotation);
+        Instantiate(cannonBall, shootPos.position, transform.rotation);
 
     }
 }
