724099a7517f3e6d968736dd999f701728a7dc6c
Warmachiene0630 <lmeza0630@gmail.com>
2025-02-11 22:13:08 -0700

    Commit for final push
    Commit has working movable walls and cannon as well as Lava pit and a key for objectives

--- ProjectGame/Project Game/Assets/Scripts Luis/Movable Wall.cs
+++ ProjectGame/Project Game/Assets/Scripts Luis/Movable Wall.cs
@@ -4,9 +4,13 @@ using UnityEngine.AI;
 
 public class MovableWall : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
+    enum wallType {forward, up }
 
-    private float trapTimer;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] wallType type;
+
+    [SerializeField] float trapTimer;
+    [SerializeField] float trapDelay;
 
     //timer count
     [SerializeField] float trapRate;
@@ -14,12 +18,10 @@ public class MovableWall : MonoBehaviour
     [SerializeField] int trapSpeed;
 
     private Vector3 playerDistance;
-    private bool trapMoved;
+    bool trapMoved; 
     [SerializeField] int trapTrigger;
 
     Vector3 trapVel;
-
-    Vector3 trapStart;
 
     private bool active;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -34,26 +36,69 @@ public class MovableWall : MonoBehaviour
         playerDistance = transform.position;
 
         trapTimer += Time.deltaTime;
-
-        moveTrap();
+        if (type == wallType.forward)
+        {
+            moveTrapFor();
+        }else if (type == wallType.up)
+        {
+            if (trapMoved == false) {
+                moveTrapUp();
+            }
+            else
+            {
+                if (trapTimer >= trapDelay)
+                {
+                    trapMoved = false;
+                    trapTimer = 0;
+                }
+            }
+        }
+       
 
         
 
     }
 
-    private void moveTrap()
+    private void moveTrapFor()
     {
+       if (trapTimer <= trapRate)
+       {
+           rb.linearVelocity = transform.forward * trapSpeed;
+       }
+       else
+       {
+           rb.linearVelocity = transform.forward * -trapSpeed;
+           if (trapTimer >= (2 * trapRate))
+           {
+               trapTimer = 0;
+           }
+       }
+    }
+
+    public void moveTrapUp()
+    {
+       
         if (trapTimer <= trapRate)
         {
-            trapVel.x = trapSpeed;
+           rb.linearVelocity = transform.up * trapSpeed;
         }
         else
         {
-            trapVel.x = -trapSpeed;
-            if (trapTimer == 2 * trapRate)
-            {
+           if (trapTimer <= (3 * trapRate))
+           {
+              rb.linearVelocity = transform.up * 0;
+           }
+           else if(trapTimer <= (4 * trapRate))
+           {
+                rb.linearVelocity = -transform.up * trapSpeed;
+
+           }
+           else if(trapTimer <= (5 * trapRate))
+           {
                 trapTimer = 0;
-            }
-        }
+                trapMoved = true;                    
+           }
+       }
     }
+
 }
