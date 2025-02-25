8d605d1d9f1be511d9e9b62513fd858c169ae6c4
Warmachiene0630 <lmeza0630@gmail.com>
2025-02-24 19:26:09 -0700

    Included Spawner

--- ProjectGame/Project Game/Assets/Scripts Luis/Spawner.cs
+++ ProjectGame/Project Game/Assets/Scripts Luis/Spawner.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class spawner : MonoBehaviour
 {
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] int numToSpawn;
+    [SerializeField] int timeBetweenSpawns;
+    [SerializeField] Transform[] spawnPos;
+
+    float spawnTimer;
+
+    int spawnCount;
+
+    bool startSpawning;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,38 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTimer += Time.deltaTime;
+        if (startSpawning == true)
+        {
+            if (spawnCount <= numToSpawn && spawnTimer >= timeBetweenSpawns)
+            {
+                spawn();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = false;
+        }
+    }
+
+    void spawn()
+    {
+        int arrPos = Random.Range(0, spawnPos.Length);
+
+        Instantiate(objectToSpawn, spawnPos[arrPos].position, spawnPos[arrPos].rotation);
+        spawnCount++;
+        spawnTimer = 0;
     }
 }
