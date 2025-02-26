06569b39c8f4fb3853ac56534f3b254ca4e37a73
Warmachiene0630 <lmeza0630@gmail.com>
2025-02-25 20:03:43 -0700

    Updating Main

--- ProjectGame/Project Game/Assets/Scripts Luis/Key.cs
+++ ProjectGame/Project Game/Assets/Scripts Luis/Key.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-
+    private void Start()
+    {
+        GameManager.instance.updateGameGoal(1);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        GameManager.instance.updateGameGoal(1);
+        GameManager.instance.updateGameGoal(-1);
         Destroy(gameObject);
-
     }
 }
