f023de6300e5f38b34499c89b979b978575eeffc
Audra McFerran <almcferran@student.fullsail.edu>
2025-02-26 22:30:22 -0500

    Fixed game goal count when enemies spawn

--- ProjectGame/Project Game/Assets/Scripts - Audra/Spawner.cs
+++ ProjectGame/Project Game/Assets/Scripts - Audra/Spawner.cs
@@ -16,7 +16,7 @@ public class Spawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameManager.instance.updateGameGoal(numToSpawn);
+
     }
 
     // Update is called once per frame
